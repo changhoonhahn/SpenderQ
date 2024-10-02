3e4172f3e8d386bd7dee8c7bcf0e049a1eb77a4d
changhoonhahn <changhoon.hahn@princeton.edu>
2024-10-02 16:58:02 -0400

    lyalpha.py functions for identifying lya absoprtion
    __init__.py now has a function to load models

--- src/spenderq/__init__.py
+++ src/spenderq/__init__.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+import torch
+from torch import nn
+from spender import SpectrumAutoencoder
+
+from .desi_qso import DESI 
+
 
 __all__ = [""]
 
@@ -7,3 +13,45 @@ __email__ = "changhoon.hahn@princeton.edu"
 __uri__ = "https://github.com/changhoonhahn/SpenderQ"
 __license__ = "MIT"
 __copyright__ = "Copyright 2024 ChangHoon Hahn"
+
+
+def load_model(filename, instruments=None, latents=10, z_min=2.1, z_max=3.5, bins=9780,
+        n_hidden=(64, 256, 1024)):
+    ''' load spenderq model from file 
+    '''
+    if instruments is None: 
+        instruments = [DESI()]
+
+    # restframe wavelength for reconstructed spectra
+    # Note: represents joint dataset wavelength range
+    lmbda_min = instruments[0].wave_obs[0]/(1.0+z_max) # 2000 A
+    lmbda_max = instruments[0].wave_obs[-1]/(1.0+z_min) # 9824 A
+    wave_rest = torch.linspace(lmbda_min, lmbda_max, bins, dtype=torch.float32)
+
+    # define and train the model
+    models = [ SpectrumAutoencoder(instrument,
+                                   wave_rest,
+                                   n_latent=latents,
+                                   n_hidden=n_hidden,
+                                   act=[nn.LeakyReLU()]*(len(n_hidden)+1)
+                                   )
+              for instrument in instruments ]
+
+    device = instruments[0].wave_obs.device
+    model_struct = torch.load(filename, map_location=device)
+    
+    for i, model in enumerate(models):
+        # backwards compat: encoder.mlp instead of encoder.mlp.mlp
+        if 'encoder.mlp.mlp.0.weight' in model_struct['model'][i].keys():
+            from collections import OrderedDict
+            model_struct['model'][i] = OrderedDict([(k.replace('mlp.mlp', 'mlp'), v) for k, v in model_struct['model'][i].items()])
+        # backwards compat: add instrument to encoder
+        try:
+            model.load_state_dict(model_struct['model'][i], strict=False)
+        except RuntimeError:
+            model_struct['model'][i]['encoder.instrument.wave_obs']= instruments[i].wave_obs
+            #model_struct['model'][i]['encoder.instrument.skyline_mask']= instruments[i].skyline_mask
+            model.load_state_dict(model_struct[i]['model'], strict=False)
+
+    losses = model_struct['losses']
+    return models, losses
--- src/spenderq/lyalpha.py
+++ src/spenderq/lyalpha.py
@@ -1,48 +1,51 @@
 '''
 
-methods for SpenderQ 
+methods for identifying LyA absorption 
 
 
 '''
 import numpy as np 
-import util as U
+from . import util as U
 
 
-
-def identify_LyA(wobs, fobs, iobs, zobs, wmod, fmod, sigma=2, method='rebin', verbose=False):
+def identify_LyA(wobs, fobs, iobs, zobs, wmod, fmod, sigma=2, wmax=None, method='rebin', verbose=False):
     ''' identify wavelengths with LyA absorption based on a reconstructed spectra.
-
-
-
     '''
-    w_lya = 1215.67 * (1. + zobs) # observed wavelength of LyA
+    if wmax is None: 
+        wmax = 1215.67 * (1. + zobs) # observed wavelength of LyA
 
     if method == 'rebin':
         # identify LyA regions after rebinning the observed spectra
-
-        # rebin to coarser wavelength
-        w_coarse = wobs[::5]
-        # observed flux and ivar coarse binned
-        fobs_coarse = U.trapz_rebin(wobs, fobs, edges=w_coarse)
-        ivar_coarse = U.trapz_rebin(wobs, iobs/np.gradient(wobs), edges=w_coarse) * (w_coarse[1:] - w_coarse[:-1])
-        # reconstructed flux coarse binned
-        fmod_coarse = U.trapz_rebin(wmod, fmod, edges=w_coarse)
-
-        is_absorb_coarse = np.zeros(len(w_coarse)-1).astype(bool)
-
-        # below LyA
-        below_lya = (w_coarse[1:] < w_lya)
-        is_absorb_coarse[below_lya] = (fmod_coarse[below_lya] - fobs_coarse[below_lya] > sigma * ivar_coarse[below_lya]**-0.5)
-        # above LyA (more conservative 3 sigma clipping)
-        is_absorb_coarse[~below_lya] = (fmod_coarse[~below_lya] - fobs_coarse[~below_lya] > 3 * ivar_coarse[~below_lya]**-0.5)
-
-        is_absorb = np.zeros(len(wobs)).astype(bool)
-        is_absorb[:-4][::5] = is_absorb_coarse
-        is_absorb[1:-3][::5] = is_absorb_coarse
-        is_absorb[2:-2][::5] = is_absorb_coarse
-        is_absorb[3:-1][::5] = is_absorb_coarse
-        is_absorb[4:][::5] = is_absorb_coarse
+        return _id_LyA_rebin(wobs, fobs, iobs, zobs, wmod, fmod, 
+                sigma=sigma, wmax=wmax, verbose=verbose)
     else:
         raise NotImplementedError
 
-    return is_absorb
+
+def _id_LyA_rebin(wobs, fobs, iobs, zobs, wmod, fmod, sigma=2, wmax=None, verbose=False):
+    ''' coarsely rebin spectrum and identify absorption wavelengths
+    '''
+    # rebin to coarser wavelength
+    w_coarse = wobs[::5]
+    # observed flux and ivar coarse binned
+    fobs_coarse = U.trapz_rebin(wobs, fobs, edges=w_coarse)
+    ivar_coarse = U.trapz_rebin(wobs, iobs/np.gradient(wobs), edges=w_coarse) * (w_coarse[1:] - w_coarse[:-1])
+    # reconstructed flux coarse binned
+    fmod_coarse = U.trapz_rebin(wmod, fmod, edges=w_coarse)
+
+    is_absorb_coarse = np.zeros(len(w_coarse)-1).astype(bool)
+
+    # below LyA
+    below_lya = (w_coarse[1:] < wmax)
+    is_absorb_coarse[below_lya] = (fmod_coarse[below_lya] - fobs_coarse[below_lya] > sigma * ivar_coarse[below_lya]**-0.5)
+    # above LyA (more conservative 3 sigma clipping)
+    is_absorb_coarse[~below_lya] = (fmod_coarse[~below_lya] - fobs_coarse[~below_lya] > 3 * ivar_coarse[~below_lya]**-0.5)
+
+    is_absorb = np.zeros(len(wobs)).astype(bool)
+    is_absorb[:-4][::5] = is_absorb_coarse
+    is_absorb[1:-3][::5] = is_absorb_coarse
+    is_absorb[2:-2][::5] = is_absorb_coarse
+    is_absorb[3:-1][::5] = is_absorb_coarse
+    is_absorb[4:][::5] = is_absorb_coarse
+
+    return is_absorb 
